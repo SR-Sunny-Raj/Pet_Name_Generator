ca03a1bf2d14915cb425686e6b000165d865494d
Sunny <sraj1333.sr@gmail.com>
2023-02-23 16:30:01 +0530

    key removed

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,8 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: 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,
-  // apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
